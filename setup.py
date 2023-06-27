df0737673fc1258382fd93a6d96cd965eba28d6d
Giuseppe Crinò <giuscri@gmail.com>
2023-06-27 12:26:52 +0200

    add `tinygo` executable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tinygo',
+    version='0.1.0',
+    packages=find_packages(),
+    scripts=['scripts/tinygo'],
+    install_requires=[],
+)
